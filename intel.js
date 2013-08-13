a20522a59059f7d02e4c0a8bcc794a890307d48c
bendouglas <ben@bldouglas.com>
2013-08-13 00:27:06 -0400

    First update!

---
+++ intel.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+	//	$("body").html("jQuery is working");	
+	setTimeout(function() {$(".chris_section #col1 .task_box").addClass("chris");},100)
+	setTimeout(function() {$(".chris_section #col2 .task_box").addClass("chris");},200)
+	setTimeout(function() {$(".chris_section #col3 .task_box").addClass("chris");},300)
+	setTimeout(function() {$(".chris_section #col4 .task_box").addClass("chris");},400)
+	setTimeout(function() {$(".chris_section #col5 .task_box").addClass("chris");},500)
+	setTimeout(function() {$(".chris_section #col6 .task_box").addClass("chris");},600)
+	setTimeout(function() {$(".chris_section #col7 .task_box").addClass("chris");},700)
+	setTimeout(function() {$(".chris_section #col8 .task_box").addClass("chris");},800)
+	setTimeout(function() {$(".ben_section #col1 .task_box").addClass("ben");},900)
+	setTimeout(function() {$(".ben_section #col2 .task_box").addClass("ben");},1000)
+	setTimeout(function() {$(".ben_section #col3 .task_box").addClass("ben");},1100)
+	setTimeout(function() {$(".ben_section #col4 .task_box").addClass("ben");},1200)
+	setTimeout(function() {$(".ben_section #col5 .task_box").addClass("ben");},1300)
+	setTimeout(function() {$(".ben_section #col6 .task_box").addClass("ben");},1400)
+	setTimeout(function() {$(".ben_section #col7 .task_box").addClass("ben");},1500)
+	setTimeout(function() {$(".ben_section #col8 .task_box").addClass("ben");},1600)
+
+
+});
